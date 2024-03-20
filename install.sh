53d9a78c43a8a72e02bf560f9d1ce30fd080462a
Ku Ji Heon <96341900+GucciHeon@users.noreply.github.com>
2024-03-20 14:51:20 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,13 +24,13 @@ add_updater()
     echo -n "Adding update manager to moonraker.conf... "
     update_section=$(grep -c '\[update_manager[a-z ]* MA3D\]' $MOONRAKER_CONFIG || true)
     if [ "$update_section" -eq 0 ]; then
-        echo -e "\n[update_manager MA3D]" >> "$MOONRAKER_CONFIG"
+        echo "\n[update_manager MA3D]" >> "$MOONRAKER_CONFIG"
         echo "type: git_repo" >> "$MOONRAKER_CONFIG"
-        echo "path: ${SRCDIR}" >> "$MOONRAKER_CONFIG"
+        echo "path: ${HOME}/MA3D" >> "$MOONRAKER_CONFIG"
         echo "primary_branch: main" >> "$MOONRAKER_CONFIG"
         echo "origin: https://oauth2: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@github.com/MadeAll/MA3D.git" >> "$MOONRAKER_CONFIG"
-        echo -e "\n" >> "$MOONRAKER_CONFIG"
         echo "is_system_service: False" >> "$MOONRAKER_CONFIG"
+        echo "\n" >> "$MOONRAKER_CONFIG"
         echo "[OK]"
 
         echo -n "Restarting Moonraker... "
