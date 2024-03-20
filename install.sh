c30af8ce4752c070e5dcf67ced84a3e5b59f3b8d
Ku Ji Heon <96341900+GucciHeon@users.noreply.github.com>
2024-03-20 16:46:29 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,9 +40,8 @@ add_updater()
     echo "type: git_repo" >> "$MOONRAKER_CONFIG"
     echo "path: ~/MA3D" >> "$MOONRAKER_CONFIG"
     echo "primary_branch: main" >> "$MOONRAKER_CONFIG"
-    echo "origin: https://github.com/MadeAll/MA3D.git" >> "$MOONRAKER_CONFIG"
-    echo "is_system_service: False" >> "$MOONRAKER_CONFIG"
-    echo "managed_services: MA3D" >> "$MOONRAKER_CONFIG"
+    echo "origin: https://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@github.com/MadeAll/MA3D.git" >> "$MOONRAKER_CONFIG"
+    echo "managed_service: MA3D" >> "$MOONRAKER_CONFIG"
     echo "\n" >> "$MOONRAKER_CONFIG"
     echo "[OK]"
 
