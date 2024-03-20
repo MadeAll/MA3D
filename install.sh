89a81b52823cd70fd604de51da106bb2753f8384
Ku Ji Heon <96341900+GucciHeon@users.noreply.github.com>
2024-03-20 16:01:53 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,24 +21,33 @@ verify_ready()
 
 add_updater()
 {
-    echo -n "Adding update manager to moonraker.conf... "
-    update_section=$(grep -c '\[update_manager[a-z ]* MA3D\]' $MOONRAKER_CONFIG || true)
-    if [ "$update_section" -eq 0 ]; then
-        echo "\n[update_manager MA3D]" >> "$MOONRAKER_CONFIG"
-        echo "type: git_repo" >> "$MOONRAKER_CONFIG"
-        echo "path: ${HOME}/MA3D" >> "$MOONRAKER_CONFIG"
-        echo "primary_branch: main" >> "$MOONRAKER_CONFIG"
-        echo "origin: https://oauth2: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@github.com/MadeAll/MA3D.git" >> "$MOONRAKER_CONFIG"
-        echo "is_system_service: False" >> "$MOONRAKER_CONFIG"
-        echo "\n" >> "$MOONRAKER_CONFIG"
-        echo "[OK]"
+    echo -n "Checking for existing update manager in moonraker.conf... "
+    # Define pattern to search for the existing updater section
+    start_pattern='^\[update_manager[a-z ]* MA3D\]'
+    end_pattern='^\['
+    # Use awk to remove the existing section if it exists
+    # This awk script checks lines between start_pattern and the next section start (end_pattern)
+    # and excludes them from the output.
+    awk -v start="$start_pattern" -v end="$end_pattern" '
+        $0 ~ start {flag=1; next}
+        $0 ~ end && flag {flag=0}
+        !flag
+    ' "$MOONRAKER_CONFIG" > temp_moonraker.conf && mv temp_moonraker.conf "$MOONRAKER_CONFIG"
 
-        echo -n "Restarting Moonraker... "
-        sudo systemctl restart moonraker
-        echo "[OK]"
-    else
-        echo "[SKIPPED]"
-    fi
+    echo -n "Adding or updating update manager in moonraker.conf... "
+    # Now append the new updater configuration
+    echo "\n[update_manager MA3D]" >> "$MOONRAKER_CONFIG"
+    echo "type: git_repo" >> "$MOONRAKER_CONFIG"
+    echo "path: ~/MA3D" >> "$MOONRAKER_CONFIG"
+    echo "primary_branch: main" >> "$MOONRAKER_CONFIG"
+    echo "origin: https://github.com/MadeAll/MA3D.git" >> "$MOONRAKER_CONFIG"
+    echo "is_system_service: False" >> "$MOONRAKER_CONFIG"
+    echo "\n" >> "$MOONRAKER_CONFIG"
+    echo "[OK]"
+
+    echo -n "Restarting Moonraker... "
+    sudo systemctl restart moonraker
+    echo "[OK]"
 }
 
 # Run steps
