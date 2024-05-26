d835c6f714450a24461ee8584febc58c04e8780b
Gucciheon <gucciheon@gmail.com>
2024-05-26 20:11:15 +0900

    bugfix

--- api_handler.py
+++ api_handler.py
@@ -15,10 +15,9 @@ def main(log, topic, message):
     global logger
     logger = log
     try:
-        message_dict = json.loads(message)  # 메시지 문자열을 딕셔너리로 변환
-        res = {}  # 응답을 위한 딕셔너리 초기화
+        message_dict = json.loads(message)
+        res = {}
 
-        # topic 기준으로 명령어를 정리해서 보냄. 이 내용으로 아래 코드를 수정해야함.
         if "/CUSTOM" in topic:
             if "/getStatus" in topic:
                 res["message"] = getStatus()
@@ -59,17 +58,15 @@ def main(log, topic, message):
             else:
                 logger.warning("No path found after /webrtc in topic")
 
-        return json.dumps(res)  # JSON 문자열로 변환하여 반환
+        return json.dumps(res)
     except Exception as e:
         return json.dumps({"error": str(e)})
 
 
 def getStatus():
     try:
-
         stat = requests.get(localhost + "/printer/objects/query?print_stats")
-        stat = stat.json()  # 응답을 JSON 딕셔너리로 변환
-        # 필요한 데이터 추출
+        stat = stat.json()
         print_stats = stat.get("result", {}).get("status", {}).get("print_stats", {})
         status = print_stats.get("state")
         filename = print_stats.get("filename")
@@ -77,12 +74,11 @@ def getStatus():
 
         klippy_stat = requests.get(localhost + "/server/info")
         klippy_stat = klippy_stat.json()
-        if (klippy_stat.get("result", {}).get("klippy_state", {}) == "shutdown"):
+        if klippy_stat.get("result", {}).get("klippy_state", {}) == "shutdown":
             status = "shutdown"
 
         temp = requests.get(localhost + "/api/printer")
-        temp = temp.json()  # 응답을 JSON 딕셔너리로 변환
-        # 필요한 데이터 추출
+        temp = temp.json()
         temp_stats = temp.get("temperature", {})
         nozzle_temp = temp_stats.get("tool0").get("actual")
         nozzle_target = temp_stats.get("tool0").get("target")
@@ -90,25 +86,20 @@ def getStatus():
         bed_target = temp_stats.get("bed").get("target")
 
         print = requests.get(localhost + "/server/files/metadata?filename=" + filename)
-        print = print.json()  # 응답을 JSON 딕셔너리로 변환
-        # 필요한 데이터 추출
+        print = print.json()
         file_stats = print.get("result", {})
         estimated_time = file_stats.get("estimated_time")
 
-        # 스냅샷 이미지 가져오기 및 처리
         snapshot_response = requests.get(localhost + "/webcam/?action=snapshot")
         if snapshot_response.status_code == 200:
             img = Image.open(BytesIO(snapshot_response.content))
-            img_resized = img.resize(
-                (300, int((img.height / img.width) * 300))
-            )  # 가로 400px 비율 유지하여 리사이즈
+            img_resized = img.resize((300, int((img.height / img.width) * 300)))
             buffered = BytesIO()
             img_resized.save(buffered, format="JPEG")
             snapshot_base64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
         else:
-            snapshot_base64 = None  # 이미지를 가져오지 못한 경우
+            snapshot_base64 = None
 
-        # 새로운 딕셔너리 생성
         extracted_data = {
             "status": status,
             "filename": filename,
@@ -121,38 +112,32 @@ def getStatus():
             "snapshot": snapshot_base64,
         }
 
-        return json.dumps(extracted_data)  # JSON 문자열로 변환하여 반환
+        return json.dumps(extracted_data)
     except Exception as e:
         return json.dumps({"error": str(e)})
 
 
 def uploadFile(filename, download_url):
     try:
-        # 파일 저장 경로 설정
         save_path = f"/home/biqu/printer_data/gcodes/{filename}"
 
-        # Firebase Storage에서 파일 다운로드
         response = requests.get(download_url, stream=True)
         if response.status_code == 200:
-            # 로컬에 파일 저장
             with open(save_path, "wb") as file:
                 for chunk in response.iter_content(chunk_size=1024):
                     file.write(chunk)
-            # 성공 메시지 반환
             return json.dumps({"message": "File successfully downloaded"})
         else:
-            # 다운로드 실패 시 오류 메시지 반환
             return json.dumps({"error": "Failed to download file"})
     except Exception as e:
-        # 예외 발생 시 오류 메시지 반환
         return json.dumps({"error": str(e)})
 
 
 def request_GET(url):
     try:
         response = requests.get(localhost + url)
-        response = response.json()  # 응답을 JSON 딕셔너리로 변환
-        return json.dumps(response["result"])  # JSON 문자열로 변환하여 반환
+        response = response.json()
+        return json.dumps(response["result"])
     except Exception as e:
         return json.dumps({"error": str(e)})
 
@@ -160,8 +145,8 @@ def request_GET(url):
 def request_POST(url):
     try:
         response = requests.post(localhost + url)
-        response = response.json()  # 응답을 JSON 딕셔너리로 변환
-        return json.dumps(response["result"])  # JSON 문자열로 변환하여 반환
+        response = response.json()
+        return json.dumps(response["result"])
     except Exception as e:
         return json.dumps({"error": str(e)})
 
@@ -169,8 +154,8 @@ def request_POST(url):
 def request_DELETE(url):
     try:
         response = requests.delete(localhost + url)
-        response = response.json()  # 응답을 JSON 딕셔너리로 변환
-        return json.dumps(response["result"])  # JSON 문자열로 변환하여 반환
+        response = response.json()
+        return json.dumps(response["result"])
     except Exception as e:
         return json.dumps({"error": str(e)})
 
@@ -228,9 +213,9 @@ async def handle_candidate(logger, pc, candidate):
     try:
         logger.info("Adding ICE candidate: %s", candidate)
         ice_candidate = RTCIceCandidate(
-            sdpMid=candidate["sdpMid"],
-            sdpMLineIndex=candidate["sdpMLineIndex"],
-            candidate=candidate["candidate"],
+            sdpMid=candidate.get("sdpMid"),
+            sdpMLineIndex=candidate.get("sdpMLineIndex"),
+            candidate=candidate.get("candidate"),
         )
         await pc.addIceCandidate(ice_candidate)
         logger.info("ICE candidate added")
@@ -255,11 +240,11 @@ async def gather_ice_candidates(logger, pc):
     logger.info("ICE gathering state is now complete")
 
 
-pc = None  # 전역 RTCPeerConnection 객체
+pc = None
 
 
 def request_webRTC(url, message):
-    global pc  # 전역 객체 사용
+    global pc
     try:
         logger.info("request_webRTC called with url: %s and message: %s", url, message)
 
@@ -274,7 +259,7 @@ def request_webRTC(url, message):
             stun_servers = [
                 {"urls": "stun:stun.l.google.com:19302"},
                 {"urls": "stun:stun1.l.google.com:19302"},
-                {"urls": "stun:stun2.l.google.com:19302"}
+                {"urls": "stun:stun2.l.google.com:19302"},
             ]
 
             pc = RTCPeerConnection(configuration={"iceServers": stun_servers})
@@ -320,9 +305,8 @@ def request_webRTC(url, message):
             if not pc:
                 raise Exception("No peer connection found")
 
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            logger.info("Event loop created and set")
+            loop = asyncio.get_event_loop()
+            logger.info("Using existing event loop")
             loop.run_until_complete(handle_candidate(logger, pc, data))
             logger.info("Added ICE candidate to pc")
 
