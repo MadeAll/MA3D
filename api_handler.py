c7f90d5f2bba827f415fe6f194203a902215b825
Gucciheon <gucciheon@gmail.com>
2024-05-26 18:54:56 +0900

    get video from crowsnest

--- api_handler.py
+++ api_handler.py
@@ -180,7 +180,7 @@ class WebcamStreamTrack(MediaStreamTrack):
 
     def __init__(self):
         super().__init__()
-        self.player = MediaPlayer("/dev/video9")
+        self.player = MediaPlayer("http://localhost/webcam/?action=stream")
 
     async def recv(self):
         frame = await self.player.video.recv()
